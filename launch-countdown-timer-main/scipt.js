ad56e530752d3ed24e23bad3033af09796d5ea5d
onlykingKD <kaustubhdwivedi1729@gmail.com>
2021-08-08 01:57:28 +0530

    Made the timer

---
+++ launch-countdown-timer-main/scipt.js
@@ -0,0 +1,31 @@
+
+var timer = setInterval(function() {
+
+const destination = new Date("2022-01-01T00:00:00");
+const current = new Date();
+
+const diff = (destination .getTime() - current.getTime()) / 1000;
+
+const months = 12 - current.getMonth() - 1;
+
+const x = new Date(current.getFullYear(), current.getMonth(), 0).getDate();
+const y = new Date(current.getFullYear(), current.getMonth(), current.getDate()).getDate();
+
+const days = x - y;
+const hours = Math.floor(diff % (24 * 60 * 60) / (60 * 60));
+const minutes = Math.floor(diff % (60 * 60) / 60);
+const seconds = Math.floor(diff % 60);
+
+document.getElementById("seconds").innerText = seconds;
+document.getElementById("minutes").innerText = minutes;
+document.getElementById("hours").innerText = hours;
+document.getElementById("days").innerText = days;
+document.getElementById("months").innerText = months;
+}, 1000);
+
+
+// console.log(months);
+// console.log(days);
+// console.log(hours);
+// console.log(minutes);
+// console.log(seconds);
