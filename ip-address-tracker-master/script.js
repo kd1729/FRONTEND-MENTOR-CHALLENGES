c5796062089c3b4e9a4a91b02e8bda15ea519212
onlykingKD <kaustubhdwivedi1729@gmail.com>
2021-08-10 02:26:56 +0530

    1

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -21,16 +21,16 @@ function onMapClick(e) {
 mymap.on('click', onMapClick);
 
 
-// var IP = document.getElementById("search");
-// var ip = IP.value;
-// var api_key = "at_q49o2j7ZW4KSwREIkNEiEgz6sqrZ6";
+var IP = document.getElementById("search");
+var ip = IP.value;
+var api_key = "at_q49o2j7ZW4KSwREIkNEiEgz6sqrZ6";
 
-// function searchIP () {
-//     $.ajax({
-//         url: "https://geo.ipify.org/api/v1",
-//         data: { apiKey: api_key, ipAddress: ip },
-//         success: function (data) {
-//             $("body").append("<pre>" + JSON.stringify(data, "", 2) + "</pre>");
-//         }
-//     });
-// };
+function searchIP () {
+    $.ajax({
+        url: "https://geo.ipify.org/api/v1",
+        data: { apiKey: api_key, ipAddress: ip },
+        success: function (data) {
+            $("body").append("<pre>" + JSON.stringify(data, "", 2) + "</pre>");
+        }
+    });
+};
