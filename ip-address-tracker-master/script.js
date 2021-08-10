a453a9ac65317a5f35f1c3a34895ec5e3509114d
onlykingKD <kaustubhdwivedi1729@gmail.com>
2021-08-11 01:18:59 +0530

    Added Marker

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -1,6 +1,6 @@
 // Mapping API
 
-var mymap = L.map('mapid').setView([26.41, 82.54], 10);
+var mymap = L.map('mapid').setView([26.41, 82.54], 5);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -11,59 +11,27 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: 'pk.eyJ1Ijoib25seWtpbmdrZCIsImEiOiJja3MzbnZyZmExeHk0MnZtcmMwdjB6b3V5In0.YGSFJvrGINqywHieF1IXBQ'
 }).addTo(mymap);
 
-var popup = L.popup();
 
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent("You clicked the map at " + e.latlng.toString())
-    .openOn(mymap);
-}
+var IP_ADD = document.getElementById("ip-address");
+var LOC = document.getElementById("location");
+var TIMEZONE = document.getElementById("timezone");
+var ISP = document.getElementById("ISP");
 
-
-mymap.on('click', onMapClick);
-
-
-
-
-
-
-
-
-// IP Geolocation API
-
-
-
-
-// var api_key = "at_q49o2j7ZW4KSwREIkNEiEgz6sqrZ6";
-
-// $("#search").click(function () {
-//     $.ajax({
-//         url: "https://swapi.dev/api/films/",
-//         // data: {apiKey: api_key, ipAddress: ip},
-//         success: function(data) {
-//             // $("body").append("<pre>"+ JSON.stringify(data,"",2)+"</pre>");
-//             console.log(data);
-//         }
-//     });
-//  });
-
-
-
-// alert(url);
 
 function searchIP() {
+  window.scrollBy(0,300);
   var IP = document.getElementById("search");
   var ip = IP.value;
   var url = "https://geo.ipify.org/api/v1?apiKey=at_q49o2j7ZW4KSwREIkNEiEgz6sqrZ6&ipAddress=" + ip;
   fetch(url)
-  .then(response => response.json())
-  .then(json => console.log(json));
+    .then(response => response.json())
+    .then((data) => {
+      IP_ADD.innerText = data.ip;
+      LOC.innerText = data.location.city + " " + data.location.region;
+      TIMEZONE.innerText = data.location.timezone;
+      ISP.innerText = data.isp;
+      var marker = L.marker([data.location.lat, data.location.lng]).addTo(mymap);
+      marker.bindPopup("<b>" + data.location.lat + "<br>" + data.location.lng + "</b>").openPopup();
+    });
 }
 
-
-// async  function searchIP() {
-//     let response = await fetch("https://jsonplaceholder.typicode.com/todos/")
-//     let data = await response.json();
-//     console.log(data);
-//     }
